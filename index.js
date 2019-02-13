88eef6ff08e49080de60720ebbdf4e9b2106da35
Josh Lukas <joshuah.lukas@gmail.com>
2019-02-12 21:22:08 -0500

    Added attachment example

--- index.js
+++ index.js
@@ -20,12 +20,39 @@ function verify(token, challenge, callback) {
 function reply(event, callback) {
   // check for non-bot message and keywords
   if (!event.subtype && event.type === 'message' && /(aws|lambda)/ig.test(event.text)) {
-    const text = `<@${event.user}> isn't AWS Lambda awesome?`;
+    const botText = `<@${event.user}> isn't AWS Lambda awesome?`;
+    const attachment = [
+      {
+        fallback: 'Required plain-text summary of the attachment.',
+        color: '#2eb886',
+        pretext: 'Optional text that appears above the attachment block',
+        author_name: 'Bobby Tables',
+        author_link: 'http://flickr.com/bobby/',
+        author_icon: 'http://flickr.com/icons/bobby.jpg',
+        title: 'Slack API Documentation',
+        title_link: 'https://api.slack.com/',
+        text: 'Optional text that appears within the attachment',
+        fields: [
+          {
+            title: 'Priority',
+            value: 'High',
+            short: false,
+          },
+        ],
+        image_url: 'http://my-website.com/path/to/image.jpg',
+        thumb_url: 'http://example.com/path/to/thumb.png',
+        footer: 'Slack API',
+        footer_icon: 'https://platform.slack-edge.com/img/default_application_icon.png',
+        ts: 123456789,
+      },
+    ];
+    // const myJSON = JSON.stringify(attachment);
     const message = {
       statusCode: 200,
       token: ACCESS_TOKEN,
       channel: event.channel,
-      text,
+      text: botText,
+      attachments: JSON.stringify(attachment),
     };
     // console.log('request: ' + JSON.stringify(message));
     const query = qs.stringify(message); // prepare the querystring
@@ -38,7 +65,7 @@ function reply(event, callback) {
 // Lambda handler
 exports.handler = (data, context, callback) => {
   let body;
-  // console.log(process.env);
+  // console.log(data.body);
   if (data.body !== null && data.body !== undefined) {
     body = JSON.parse(data.body);
   }
